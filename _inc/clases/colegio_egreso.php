7e46db419ad781c9024e7f45a4378ab0aca56bd2
Hilda Quiroz <hildalux_2006@hotmail.com>
2014-04-12 17:59:02 -0400

    Registro Colegio egreso

---
+++ _inc/clases/colegio_egreso.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of colegio_egreso
+ *
+ * @author Hilda
+ */
+class Colegio_egreso  extends  Objectbase  {
+    //put your code here
+    var $nombre_colegio;
+    var $tipo_colegio;
+    var $area_colegio;
+    var $fecha_titulo_vachiller;
+    var $pais_id;
+    var $departamento_id;
+    var $provincia_id;
+    var $municipio_id;
+    var $funcionario_id;
+     
+}
+
+?>
